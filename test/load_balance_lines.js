f2464f84a39bdbce1a7fe5bc942c638ec453de9b
maxlath <k@maxlath.eu>
2018-05-04 18:23:58 +0200

    added tests with a large input

--- test/load_balance_lines.js
+++ test/load_balance_lines.js
@@ -4,15 +4,12 @@ const coreCount = require('os').cpus().length
 
 const command = `export LBL_SILENT=true; ./bin/load-balance-lines ./test/assets/double_nums < ./test/assets/nums.ndjson`
 
-const expectedTotal = 100 * 49 + 50 + 100
-
 describe('load-balance-lines', () => {
   it('should handle all the lines', done => {
     exec(command, (err, stdout, stderr) => {
       if (err) return done(err)
       if (stderr) return done(stderr)
-      const outputLines = stdout.trim().split('\n')
-      outputLines.length.should.equal(100)
+      expectedLinesCount(stdout, 100)
       done()
     })
   })
@@ -21,12 +18,7 @@ describe('load-balance-lines', () => {
     exec(command, (err, stdout, stderr) => {
       if (err) return done(err)
       if (stderr) return done(stderr)
-      const outputLines = stdout.trim().split('\n')
-      const total = outputLines.reduce((total, entry) => {
-        total += JSON.parse(entry).double
-        return total
-      }, 0)
-      total.should.equal(expectedTotal * 2)
+      expectedOperation(stdout, (100 * 49 + 50 + 100) * 2)
       done()
     })
   })
@@ -35,13 +27,7 @@ describe('load-balance-lines', () => {
     exec(command, (err, stdout, stderr) => {
       if (err) return done(err)
       if (stderr) return done(stderr)
-      const outputLines = stdout.trim().split('\n')
-      const pids = outputLines.reduce((pids, entry) => {
-        const { pid } = JSON.parse(entry)
-        pids[pid] = true
-        return pids
-      }, {})
-      Object.keys(pids).length.should.equal(coreCount)
+      expectedPidsCount(stdout)
       done()
     })
   })
@@ -60,4 +46,40 @@ describe('load-balance-lines', () => {
       done()
     })
   })
+
+  it('should work with large amount of data', function (done) {
+    this.timeout(30000)
+    const cmd = command.replace('nums.ndjson', 'many_nums.ndjson')
+    exec(cmd, { maxBuffer: 50 * 1024 * 1024 }, (err, stdout, stderr) => {
+      if (err) return done(err)
+      if (stderr) return done(stderr)
+      expectedLinesCount(stdout, 1000000)
+      expectedOperation(stdout, (100 * 49 + 50 + 100) * 2 * 10000)
+      expectedPidsCount(stdout)
+      done()
+    })
+  })
 })
+
+const expectedLinesCount = (stdout, expected) => {
+  getOutputLines(stdout).length.should.equal(expected)
+}
+
+const expectedOperation = (stdout, expectedTotal) => {
+  const total = getOutputLines(stdout).reduce((total, entry) => {
+    total += JSON.parse(entry).double
+    return total
+  }, 0)
+  total.should.equal(expectedTotal)
+}
+
+const expectedPidsCount = stdout => {
+  const pids = getOutputLines(stdout).reduce((pids, entry) => {
+    const { pid } = JSON.parse(entry)
+    pids[pid] = true
+    return pids
+  }, {})
+  Object.keys(pids).length.should.equal(coreCount)
+}
+
+const getOutputLines = stdout => stdout.trim().split('\n')
